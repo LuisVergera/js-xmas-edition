c751bf692b6a27042e87a49e0b6a3d59f47b1432
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-04-06 13:02:23 -0300

    Agrego cambios sugeridos por Fabricio Sodano

--- tarea/calculos-sueldos.js
+++ tarea/calculos-sueldos.js
@@ -1,47 +1,25 @@
-function obtenerMayorSueldo(numeros){
-    let arrayNumeros = []
+function obtenerMayorSueldo(numeros) {
+  let arrayNumeros = [];
 
-    for (let i = 0; i<numeros.length;i++){
-
-        arrayNumeros.push(numeros[i])
-    }
-    return Math.max.apply(Math,arrayNumeros)
+  for (let i = 0; i < numeros.length; i++) {
+    arrayNumeros.push(numeros[i]);
+  }
+  return Math.max.apply(Math, arrayNumeros);
 }
 
-function obtenerMenorSueldo(numeros){
-    let arrayNumeros = []
+function obtenerMenorSueldo(numeros) {
+  let arrayNumeros = [];
 
-    for (let i = 0; i<numeros.length;i++){
-
-        arrayNumeros.push(numeros[i])
-    }
-    return Math.min.apply(Math,arrayNumeros)
+  for (let i = 0; i < numeros.length; i++) {
+    arrayNumeros.push(numeros[i]);
+  }
+  return Math.min.apply(Math, arrayNumeros);
 }
 
-function obtenerPromedioAnual(numeros){
-    let acumulador = 0;
-    
-    for (let i = 0; i<numeros.length;i++){
-
-        acumulador+= numeros[i]
-    }
-return acumulador / numeros.length
+function obtenerPromedio(numeros) {
+  let acumulador = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    acumulador += numeros[i];
+  }
+  return acumulador / numeros.length;
 }
-
-function obtenerPromedioMensual(numeros){
-    let acumulador = 0;
-    const mesesEnUnAnio = 12;
-
-    for (let i = 0; i<numeros.length;i++){
-
-        acumulador+= numeros[i]
-    }
-return ((acumulador / numeros.length) / mesesEnUnAnio).toFixed(2);
-}
-
-
-
-
-
-
-
--- tarea/tarea-clase-6-sueldos.js
+++ tarea/tarea-clase-6-sueldos.js
@@ -1,122 +1,120 @@
-document.querySelector("#agregar-miembro").onclick = function(event){
-    event.preventDefault();
-    crearInputs();
+document.querySelector("#agregar-familiar").onclick = function (event) {
+  event.preventDefault();
+  crearFamiliar();
+};
+
+document.querySelector("#quitar-familiar").onclick = function (event) {
+  event.preventDefault();
+  borrarFamiliar();
+};
+
+document.querySelector("#calcular").onclick = function (event) {
+  event.preventDefault();
+  let $sueldosFamiliares = document.querySelectorAll(".sueldo");
+  const sueldo = obtenerArraySueldos($sueldosFamiliares);
+  if (validarFormulario(sueldo) === "") {
+    document.querySelector("#mayor-sueldo").textContent =
+      obtenerMayorSueldo(sueldo);
+    document.querySelector("#menor-sueldo").textContent =
+      obtenerMenorSueldo(sueldo);
+    document.querySelector("#salario-anual-promedio").textContent =
+      obtenerPromedio(sueldo);
+    document.querySelector("#salario-mensual-promedio").textContent =
+      obtenerPromedio(sueldo) / 12;
+  }
+};
+
+function crearFamiliar() {
+  const $div = document.createElement("div");
+  $div.className = "familiar";
+
+  const $label = document.createElement("label");
+  $label.textContent = "Sueldo anual del familiar";
+
+  const $input = document.createElement("input");
+  $input.type = "number";
+  $input.className = "sueldo";
+
+  $div.appendChild($label);
+  $div.appendChild($input);
+
+  const $integrantes = document.querySelector("#familiares");
+  $integrantes.appendChild($div);
 }
 
-document.querySelector("#quitar-miembro").onclick = function(event){
-    event.preventDefault()
-    borrarInputs()
+function borrarFamiliar() {
+  const $integrantes = document.querySelector(".familiar");
+  $integrantes.remove();
 }
 
-document.querySelector("#calcular").onclick = function(event){
-    event.preventDefault();  
-    let $sueldosFamiliares = document.querySelectorAll(".sueldo");   
-    const sueldo = obtenerSueldos($sueldosFamiliares)
-    if(validarFormulario(sueldo)===''){
-    document.querySelector("#mayor-sueldo").textContent = obtenerMayorSueldo(sueldo);
-    document.querySelector("#menor-sueldo").textContent = obtenerMenorSueldo(sueldo);
-    document.querySelector("#salario-anual-promedio").textContent = obtenerPromedioAnual(sueldo);
-    document.querySelector("#salario-mensual-promedio").textContent = obtenerPromedioMensual(sueldo);
+let $sueldosFamiliares = document.querySelectorAll(".sueldo");
+
+function obtenerArraySueldos(sueldosFamiliares) {
+  let $sueldosFamiliares = document.querySelectorAll(".sueldo");
+  const sueldos = [];
+
+  for (let i = 0; i < $sueldosFamiliares.length; i++) {
+    const sueldo = $sueldosFamiliares[i];
+    if (sueldo.value === "") {
+      continue;
     }
+    sueldos.push(Number(sueldo.value));
+  }
+  return sueldos;
 }
 
-function crearInputs(){
-    const $div = document.createElement('div');
-    $div.className = "integrante";
-    
-    const $label = document.createElement('label');
-    $label.textContent = "Sueldo anual del familiar";
+function validarSueldosFamiliares(sueldosFamiliares) {
+  if (sueldosFamiliares === 0) {
+    return "Debe ingresar un valor";
+  }
 
-    const $input = document.createElement('input');
-    $input.type = "number"
-    $input.className = "sueldo"
+  if (!/^[0-9]+$/.test(sueldosFamiliares)) {
+    return "Debe ingresar solo numeros";
+  }
 
-    $div.appendChild($label);
-    $div.appendChild($input);
-
-    const $integrantes = document.querySelector('#integrantes');
-    $integrantes.appendChild($div);
+  if (sueldosFamiliares < 10000) {
+    return "Deberias buscar un empleo mejor";
+  } else {
+    return "";
+  }
 }
 
-function borrarInputs(){
-    const $integrantes = document.querySelector('.integrante');
-    $integrantes.remove();
+function validarFormulario(array) {
+  const $form = document.querySelector("#calculador-sueldos");
+
+  let errorArray = [];
+  for (let i = 0; i < array.length; i++) {
+    errorArray.push(validarSueldosFamiliares(array[i]));
+  }
+
+  const errores = Object.assign({}, errorArray);
+
+  const esExito = manejarErrores(errores, $form) === 0;
+
+  if (esExito) {
+    return "";
+  }
 }
 
-let $sueldosFamiliares = document.querySelectorAll(".sueldo")
-let sueldosFamiliares = Number($sueldosFamiliares).value
+function manejarErrores(errores, form) {
+  const keys = Object.keys(errores);
+  let cantidadDeErrores = 0;
 
-function obtenerSueldos(sueldosFamiliares){
-    let $sueldosFamiliares = document.querySelectorAll(".sueldo")
-    const sueldos = []
+  keys.forEach(function (key) {
+    const error = errores[key];
 
-    for (let i= 0; i< sueldosFamiliares.length;i++){     
-        if (sueldosFamiliares[i].value===''){
-            continue
-        }        
-        sueldos.push(Number(sueldosFamiliares[i].value))
+    if (error) {
+      cantidadDeErrores++;
+      form[key].classList.add("error");
+      const $errores = document.querySelector("#errores");
+      $errores.classList.remove("error-hide");
+      const errorContainer = document.createElement("li");
+      const space = document.createElement("br");
+      errorContainer.innerText = error;
+      errorContainer.className = "error-text";
+      $errores.appendChild(errorContainer);
+      $errores.appendChild(space);
     }
-    return sueldos;
+  });
+  return cantidadDeErrores;
 }
-
-//Validaciones
-
-function validarSueldosFamiliares(sueldosFamiliares){
-    if (sueldosFamiliares===0){
-        return 'Debe ingresar un valor'
-    };
-
-    if (!/^[0-9]+$/.test(sueldosFamiliares)){
-        return 'Debe ingresar solo numeros'
-    }
-
-    if (sueldosFamiliares<10000){
-        return 'Deberias buscar un empleo mejor'
-    }else{
-    return ''}
-}
-
-function validarFormulario(array){
-    const $form = document.querySelector('#calculador-sueldos');
-
-    let errorArray = [];
-    for (let i = 0; i < array.length; i++){
-        errorArray.push(validarSueldosFamiliares(array[i]))
-    };
-
-    const errores = Object.assign({}, errorArray);
-
-    const esExito = manejarErrores(errores,$form)===0;
-
-    if (esExito){
-        return '';
-    }
-}
-
-
-//Manejo de errores
-
-function manejarErrores(errores, form){
-    const keys = Object.keys(errores);
-    let cantidadDeErrores = 0;
-
-    keys.forEach(function(key){
-        const error = errores[key];
-        
-        if(error){
-            cantidadDeErrores++;
-            form[key].classList.add('error');
-            const $errores = document.querySelector('#errores');
-            $errores.classList.remove('error-hide');
-            const errorContainer = document.createElement('li');
-            const space = document.createElement('br');
-            errorContainer.innerText = error;
-            errorContainer.className = "error-text"
-            $errores.appendChild(errorContainer);
-            $errores.appendChild(space);
-        }
-    })
-    return cantidadDeErrores;
-}
-
-
